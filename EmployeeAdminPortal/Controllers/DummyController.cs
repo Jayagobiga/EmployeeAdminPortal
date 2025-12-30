703ef9fa76c6639f7c236ac04647692fc8d5875f
gobiga <gobiga@gmail.com>
2025-12-30 16:12:18 +0530

    Added dummy API

---
+++ EmployeeAdminPortal/Controllers/DummyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DummyController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello from Dummy API!");
+        }
+    }
+}
